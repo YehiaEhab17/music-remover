19b3a1d40ed174b23abc96b26b07680d4eefdf16
yehiaeja <yehiab07@gmail.com>
2025-12-20 17:57:28 +0300

    win compat

--- misc/compile_and_run.ps1
+++ misc/compile_and_run.ps1
@@ -1,16 +1,16 @@
 # Remove old build folders
-Remove-Item -Recurse -Force dist, build
+Remove-Item -Recurse -Force dist, build -ErrorAction SilentlyContinue
 
 # Build the app
 pyinstaller `
     --onefile `
     --noupx `
-    --add-binary "../.binaries/ffmpeg;.binaries" `
+    --add-binary "../.binaries/ffmpeg.exe;.binaries" `
     --collect-all torch `
     --collect-all audio_separator `
     --copy-metadata torch `
     --copy-metadata audio_separator `
-    ../app.py
+    ..\app.py
 
 # Run the compiled executable
 & .\dist\app.exe
